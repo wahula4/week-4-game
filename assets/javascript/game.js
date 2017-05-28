0e34bb00e32aa4c281d7fb344f64399d174986c8
wahula4 <mawahula4@gmail.com>
2017-05-27 21:27:35 -0600

    all working except reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,5 @@
-
-	$(document).ready(function() {
-
-		function startGame() {
-			var counter = 0;
-			$(".data-crystalvalue").empty();
-			var crystalValue = 0;
-};
-
-	
+function reset() {
 	var counter = 0;
-	var wins = 0;
-	var losses = 0;
 
 	var blueCrystal = Math.floor((Math.random() * 12) + 1);
 	var greenCrystal = Math.floor((Math.random() * 12) + 1);
@@ -19,20 +8,38 @@
 
 	var targetNumber = Math.floor((Math.random() * 120) + 20);
 
+	$(".blue").attr("data-crystalvalue", blueCrystal);
+	$(".green").attr("data-crystalvalue", greenCrystal);
+	$(".red").attr("data-crystalvalue", redCrystal);
+	$(".yellow").attr("data-crystalvalue", yellowCrystal);
+
+   	$("#target").html("Target Number: " + targetNumber);
+
+	$("#current-total").html("Current Total: " + counter);
+   };
+	var wins = 0;
+	var losses = 0;
+
+	var counter = 0;
+
+	var blueCrystal = Math.floor((Math.random() * 12) + 1);
+	var greenCrystal = Math.floor((Math.random() * 12) + 1);
+	var redCrystal = Math.floor((Math.random() * 12) + 1);
+	var yellowCrystal = Math.floor((Math.random() * 12) + 1);
+
+	var targetNumber = Math.floor((Math.random() * 120) + 20);
 
 	$(".blue").attr("data-crystalvalue", blueCrystal);
 	$(".green").attr("data-crystalvalue", greenCrystal);
 	$(".red").attr("data-crystalvalue", redCrystal);
 	$(".yellow").attr("data-crystalvalue", yellowCrystal);
 
-    $("#target").append(targetNumber);
-
+   	$("#target").html("Target Number: " + targetNumber);
 
      $(".button").on("click", function() {
 
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
-    // We then add the crystalValue to the user's "counter" which is a global variable.
     // Every click, from every crystal adds to the global counter.
     counter += crystalValue;
     $("#current-total").html("Current Total: " + counter);
@@ -41,18 +48,14 @@
  		alert("You Win!");
  		wins++;
       $("#wins").html("Wins: " + wins);
-      startGame();
+      reset();
     }
 
-    else if (counter >= targetNumber) {
+    else if (counter > targetNumber) {
     	alert("You Lose!");
     	losses++;
        $("#losses").html("Losses: " + losses);
-       startGame();
+       reset();
     }
 
-    
-
      });
-
-});
