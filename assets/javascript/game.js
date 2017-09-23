50d45a77b2da5559a41bd5ca7fca5bbd35e9d088
wahula4 <mawahula4@gmail.com>
2017-09-23 10:51:24 -0600

    changed background photo

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,66 @@
 	var wins = 0;
 	var losses = 0;
-
 	var counter = 0;
 
-	var blueCrystal = Math.floor((Math.random() * 12) + 1);
-	var greenCrystal = Math.floor((Math.random() * 12) + 1);
-	var redCrystal = Math.floor((Math.random() * 12) + 1);
-	var yellowCrystal = Math.floor((Math.random() * 12) + 1);
+	var crystals = {
+			blue: 0,
+			red: 0,
+			yellow: 0,
+		 	green: 0
+		};
+
+	for (var prop in crystals) {
+		var randomvalue = Math.floor((Math.random() * 12) + 1);
+		crystals[prop] = randomvalue;
+		$("." + prop).attr("data-crystalvalue", randomvalue);
+	}
 
 	var targetNumber = Math.floor((Math.random() * 120) + 20);
 
+   	$("#target").html("Target Number: " + targetNumber);
+
 function reset() {
-	counter = 0;
+			counter = 0;
 
-	blueCrystal = Math.floor((Math.random() * 12) + 1);
-	greenCrystal = Math.floor((Math.random() * 12) + 1);
-	redCrystal = Math.floor((Math.random() * 12) + 1);
-	yellowCrystal = Math.floor((Math.random() * 12) + 1);
+			var crystals = {
+				blue: 0,
+				red: 0,
+				yellow: 0,
+			 	green: 0
+			};
 
-	targetNumber = Math.floor((Math.random() * 120) + 20);
+			for (var prop in crystals) {
+				var randomvalue = Math.floor((Math.random() * 12) + 1);
+				crystals[prop] = randomvalue;
+				$("." + prop).attr("data-crystalvalue", randomvalue);
+			}
 
-	$(".blue").attr("data-crystalvalue", blueCrystal);
-	$(".green").attr("data-crystalvalue", greenCrystal);
-	$(".red").attr("data-crystalvalue", redCrystal);
-	$(".yellow").attr("data-crystalvalue", yellowCrystal);
+			targetNumber = Math.floor((Math.random() * 120) + 20);
 
-   	$("#target").html("Target Number: " + targetNumber);
+		  $("#target").html("Target Number: " + targetNumber);
 
-	$("#current-total").html("Current Total: " + counter);
+			$("#current-total").html("Current Total: " + counter);
    };
-	
 
-	$(".blue").attr("data-crystalvalue", blueCrystal);
-	$(".green").attr("data-crystalvalue", greenCrystal);
-	$(".red").attr("data-crystalvalue", redCrystal);
-	$(".yellow").attr("data-crystalvalue", yellowCrystal);
 
-   	$("#target").html("Target Number: " + targetNumber);
+  $(".button").on("click", function() {
 
-     $(".button").on("click", function() {
+		    var crystalValue = ($(this).attr("data-crystalvalue"));
+		    crystalValue = parseInt(crystalValue);
+		    // Every click, from every crystal adds to the global counter.
+		    counter += crystalValue;
+		    $("#current-total").html("Current Total: " + counter);
 
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    // Every click, from every crystal adds to the global counter.
-    counter += crystalValue;
-    $("#current-total").html("Current Total: " + counter);
-
- 	if (counter === targetNumber) {
- 		alert("You Win!");
- 		wins++;
-      $("#wins").html("Wins: " + wins);
-      reset();
-    }
-
-    else if (counter > targetNumber) {
-    	alert("You Lose!");
-    	losses++;
-       $("#losses").html("Losses: " + losses);
-       reset();
-    }
-
-     });
+			 	if (counter === targetNumber) {
+			 		alert("You Win!");
+			 		wins++;
+			      $("#wins").html("Wins: " + wins);
+			      reset();
+			    }
+		    else if (counter > targetNumber) {
+		    	alert("You Lose!");
+		    	losses++;
+		       $("#losses").html("Losses: " + losses);
+		       reset();
+		    }
+  });
